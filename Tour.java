41467443c7ca9a12e9a2aae06af5998ae0f8cadc
Rikateur <50491006+Rikateur@users.noreply.github.com>
2019-05-10 17:23:57 +0200

    Update Tour.java

--- Tour.java
+++ Tour.java
@@ -3,7 +3,7 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 public class Tour extends Piece {
     
     public void bouger() {
-        if vericationCoup(x,y) {
+        if verificationCoup(x,y) {
             this.setPosition(x,y)
         }
     }
