7085c52e45f931d36b1c06a9e3e50bc215a4da22
shawn <shawn.modestine@gmail.com>
2019-05-10 17:42:22 +0200

    Piece

---
+++ Echequier.java
@@ -0,0 +1,22 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("0f3e04c6-8168-43ad-aaf2-5422f2544c0b")
+public class Echequier {
+    @objid ("05ef11d9-125a-47e7-85f9-02c2d4ce3ba5")
+    private String Couleur;
+
+    @objid ("dc29024d-1498-4850-9d5f-d1cab950ad34")
+    private int Colonne;
+
+    @objid ("e0723c9f-c3d3-4efd-b603-7dbffc3e4377")
+    private int ligne;
+
+    @objid ("254c1881-02b2-400d-8e2f-ce4801902b95")
+    public void getPosition() {
+    }
+
+    @objid ("96d1bb84-56f4-4bc9-bedf-433e2731aa82")
+    public void manger() {
+    }
+
+}
