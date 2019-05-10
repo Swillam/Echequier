f3e7e41e9145863fd439e3b54a73df742461865a
Rikateur <50491006+Rikateur@users.noreply.github.com>
2019-05-10 17:25:41 +0200

    Update Fou.java

--- Fou.java
+++ Fou.java
@@ -1,9 +1,15 @@
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("4151706f-b48b-4ed2-9755-8d54ddca20c1")
+
 public class Fou extends Piece {
+    
     @objid ("30e4f7eb-f2bd-4b5a-9252-c0d4f17f2470")
+    
     public void bouger() {
+        if verificationCoup(x,y) {
+            this.setPosition(x,y) // Si la vérification est correcte, le pion va bouger
+        }
     }
 
 }
