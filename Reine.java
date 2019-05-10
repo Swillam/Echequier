aa8b4689035f163b1c95c308e6b2bfbab1af41a0
Rikateur <50491006+Rikateur@users.noreply.github.com>
2019-05-10 17:26:28 +0200

    Update Reine.java

--- Reine.java
+++ Reine.java
@@ -1,9 +1,15 @@
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("04e9782a-7f5c-445c-8bbc-b224956bbf14")
+
 public class Reine extends Piece {
+    
     @objid ("13cfb459-74e1-4faa-b3d3-8d2bcf61c180")
+    
     public void bouger() {
+        if verificationCoup(x,y) {
+            this.setPosition(x,y)
+        }
     }
 
 }
