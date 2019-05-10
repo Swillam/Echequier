e4975432a44fc636120c36a35bc7d63a929178bf
Rikateur <50491006+Rikateur@users.noreply.github.com>
2019-05-10 17:28:23 +0200

    Update Roi.java

--- Roi.java
+++ Roi.java
@@ -1,9 +1,15 @@
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("7b09a642-e9ea-4373-943e-a44f980c49fd")
+
 public class Roi extends Piece {
+    
     @objid ("e3169dec-43ac-446e-a16a-55c2f901909c")
+    
     public void bouger() {
+         if verificationCoup(x,y) {
+            this.setPosition(x,y) // Si la vérification est correcte, le pion va bouger
+         }
     }
 
 }
