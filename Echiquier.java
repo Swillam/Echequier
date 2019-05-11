fb3a0b3596eaed12062c68918d80310a41f89222
shawn <shawn.modestine@gmail.com>
2019-05-11 10:37:28 +0200

    Many change

--- Echiquier.java
+++ Echiquier.java
@@ -1,11 +1,19 @@
 public class Echiquier 
 {
     private Piece[][] Case = new Piece[8][8];
+    private Piece pieceJouer;
+    private Piece pieceManger;
 
-    public void getPosition() {
+
+    public Echiquier()
+    {
+
     }
 
-    public void manger() {
+    public void manger(Piece pJouer,Piece pManger) 
+    {
+        pieceJouer = pJouer;
+        pieceManger = pManger;
     }
 
     public Piece getCase(int x, int y) {
@@ -13,14 +21,29 @@ public class Echiquier
         return this.Case[x][y];
     }
 
-    public void setCase(Piece p, int x, int y) {
-        // Automatically generated method. Please delete this comment before entering specific code.
+    public void setCase(Piece p, int x, int y) 
+    {
+        if(siPiecePresente(x, y)) manger(p, this.Case[x][y]);
         this.Case[x][y] = p;
     }
 
     public boolean siPiecePresente(int x, int y) 
     {
-        return true;
+        return this.Case[x][y] != null ;
+    }
+
+    /**
+     * @return the pieceJouer
+     */
+    public Piece getPieceJouer() {
+        return pieceJouer;
+    }
+
+    /**
+     * @return the pieceManger
+     */
+    public Piece getPieceManger() {
+        return pieceManger;
     }
 
 }
